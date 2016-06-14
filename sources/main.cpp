4447f589aa9fbc19a8837d1f2722ed26c84b6053
Mikhail Artemyev <artemiev.mikhail@gmail.com>
2016-06-14 11:26:16 -0500

    Surround main block with try-catch block

--- sources/main.cpp
+++ sources/main.cpp
@@ -3,15 +3,28 @@
 
 int main(int argc, char **argv)
 {
-  Parameters param(argc, argv);
+  try
+  {
+    Parameters param(argc, argv);
 
-  if (param._verbose > 1)
-    param.print_parameters();
+    if (param._verbose > 1)
+      param.print_parameters();
 
-  param.check_parameters();
+    param.check_parameters();
 
-  Compute c(param);
-  c.run();
+    Compute c(param);
+    c.run();
+  }
+  catch (const std::exception &e)
+  {
+    std::cout << e.what() << std::endl;
+    return 1;
+  }
+  catch (...)
+  {
+    std::cout << "\n\nUnknown exception\n" << std::endl;
+    return 2;
+  }
 
   return 0;
 }
